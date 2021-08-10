c2acc8532ac0dd48adca4684910b9850b5d9a8fa
whatdowhat <rhengh01@gmail.com>
2021-08-10 21:16:36 +0900

    add page, map list ...

--- src/main/java/com/whatdo/keep/repository/SystemCommonVORepository.java
+++ src/main/java/com/whatdo/keep/repository/SystemCommonVORepository.java
@@ -1,9 +1,14 @@
 package com.whatdo.keep.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.whatdo.keep.vo.SystemCommonVO;
@@ -13,5 +18,23 @@ public interface SystemCommonVORepository extends JpaRepository<SystemCommonVO, 
 
 	SystemCommonVO findBySeq(BigDecimal seq);
 	List<SystemCommonVO> findAll();
+	
+//	@Query("select u from SystemCommonVO u where 1=1")
+//	List<Map<String,Object>> findAllNativeQueryTest1();
+	
+	@Query("select "
+			+ " u.seq as seq"
+			+ " ,u.intParam01 as intParm01"
+			+ " ,u.strParam01 as strParm01"
+			+ " from SystemCommonVO u where 1=1")
+	List<Map<String,Object>> customQuery();
+	
+	@Query("select "
+			+ " u.seq as seq"
+			+ " ,u.intParam01 as intParm01"
+			+ " ,u.strParam01 as strParm01"
+			+ " from SystemCommonVO u where 1=1")
+	Page<Map<String,Object>> customQuery2(Pageable pageable);
+	
 	
 }
--- src/test/java/ser/JPATest.java
+++ src/test/java/ser/JPATest.java
@@ -2,20 +2,27 @@ package ser;
 
 import static org.junit.Assert.assertNull;
 
-import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.whatdo.keep.config.ApplicationContextConfig;
 import com.whatdo.keep.repository.SystemCommonVORepository;
 import com.whatdo.keep.repository.TestVORepository;
-import com.whatdo.keep.vo.SystemCommonVO;
-import com.whatdo.keep.vo.TestVO;
 
 
 @WebAppConfiguration
@@ -29,34 +36,55 @@ public class JPATest {
 	@Autowired
 	private SystemCommonVORepository systemCommonVORepository;
 
-	
+	private static Logger LOGGER = LoggerFactory.getLogger(JPATest.class);
+
+
 	@Test
 	public void test() {
-//		System.out.println("(((((((((((((((((((((((((((((((((");
-//		System.out.println(testVORepository == null);
-//		TestVO vo = testVORepository.findBySeq(1);
-//		System.out.println("(((((((((((((((((((((((((((((((((");
-//		System.out.println("(((((((((((((((((((((((((((((((((");
-//		System.out.println(vo.getId());
-//		System.out.println(vo.toString());
-//		System.out.println("(((((((((((((((((((((((((((((((((");
-//		System.out.println("(((((((((((((((((((((((((((((((((");
-//		
-//		
-//		
-//		SystemCommonVO t = systemCommonVORepository.findBySeq(new BigDecimal(1));
-//		System.out.println(t);
+
+		System.out.println(systemCommonVORepository.findAll());
+		LOGGER.info("test text {}", systemCommonVORepository.findAll());
 		
-//		for(int tt=0 ; tt<1000; tt++) {
-			SystemCommonVO commonVO = new SystemCommonVO();
-			commonVO.setStrParam01("value1");
-			commonVO.setStrParam02("value2");
-			commonVO.setStrParam03("value3");
-			commonVO.setIntParam01(1);
-			commonVO.setIntParam02(2);
-			commonVO.setIntParam02(3);
-			System.out.println(systemCommonVORepository.save(commonVO));	
+		List<Map<String,Object>> list = systemCommonVORepository.customQuery();
+		
+		
+		
+		Map<String,Object> my = new HashMap<String, Object>();
+		
+		my.put("test1", 1);
+		my.put("test2", "2");
+		Gson gson = new Gson();
+//		String colum = "test2";
+//		System.out.println(list.get(0).get(colum));
+//		System.out.println(list.get(0).toString());
+//		System.out.println(my);
+		JsonElement jsonElement = gson.toJsonTree(list.get(0));
+		System.out.println(jsonElement);
+		
+		int page = 0;
+		int size = 5;
+	    Pageable paging = PageRequest.of(page, size);
+	      
+	    Page<Map<String,Object>> page_result = systemCommonVORepository.customQuery2(paging);
+	    System.out.println(page_result.getTotalElements());
+	    System.out.println(page_result.getContent().size());
+	    System.out.println(page_result.getContent().get(0).get("strParm01"));
+//		
+	      
+	      
+//		for(int i = 0; i<100;i ++) {
+//		
+//			SystemCommonVO entity = new SystemCommonVO();
+//			entity.setStrParam01("i+" + String.valueOf(i));
+//			entity.setStrParam02("i+" + String.valueOf(i));
+//			entity.setStrParam03("i+" + String.valueOf(i));
+//			systemCommonVORepository.save(entity);
+//			
 //		}
+//		
+		
+		
+		//LOGGER.trace("exiting application");
 		
 		
 		assertNull(null);
